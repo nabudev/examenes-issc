0db60910cdf1a54250d98bd71ab020097c51884d
nahueldev <nabudev01@gmail.com>
2025-02-04 22:51:58 -0300

    correccion url render

--- myapp/settings.py
+++ myapp/settings.py
@@ -142,7 +142,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "https://tu-frontend.onrender.com",
+    "https://examenes-issc.onrender.com",
 ]
 
 REST_FRAMEWORK = {
